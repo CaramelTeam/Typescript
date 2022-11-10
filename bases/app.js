b7c89c8c2526ad968c5d521fa33940162f4273ef
Jim Loza <jimloza25@gmail.com>
2022-11-09 18:10:06 -0600

    first commit

---
+++ bases/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//Hola mundo con typescript
+const msg = 'Hola mundo!!';
+const hero = {
+    name: 'IronMan',
+    age: 45
+};
+hero.age = 50;
+console.log(msg);
+console.log(hero.age + 1);
---
+++ bases/app.ts
@@ -0,0 +1,9 @@
+//Hola mundo con typescript
+const msg: string = 'Hola mundo!!';
+const hero = {
+    name: 'IronMan',
+    age: 45
+}
+hero.age = 50;
+console.log(msg);
+console.log(hero.age+1);
